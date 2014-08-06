9de40b23ed9a1fb7af401fda335779262c809661
Mark Kusch <mark.kusch@gmail.com>
2014-08-06 13:45:53 +0200

    Update tests for new configuration directory structure

--- spec/default/heka_spec.rb
+++ spec/default/heka_spec.rb
@@ -4,8 +4,23 @@ describe package('heka') do
   it { should be_installed }
 end
 
-describe file('/etc/hekad.toml') do
-  it { should be_file }
+%w{
+  /etc/hekad.d
+  /var/cache/hekad
+  /var/cache/hekad/dashboard
+}.each do |f|
+  describe file(f) do
+    it { should be_directory }
+  end
+end
+
+%w{
+  /etc/hekad.d/hekad.toml
+  /etc/hekad.d/dashboard.toml
+}.each do |f|
+  describe file(f) do
+    it { should be_file }
+  end
 end
 
 describe service('hekad') do
